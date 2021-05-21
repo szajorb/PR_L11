7af5cfaec45cd5413013a0c554ddcef11101af43
bszaj <b.szajor@gmail.com>
2021-05-21 14:20:45 +0200

    opis zmian

--- app/app.js
+++ app/app.js
@@ -2,7 +2,9 @@ import bodyParser from 'body-parser';
 import config from './config';
 import cors from 'cors';
 import express from 'express';
+import mongoose from 'mongoose';
 import morgan from 'morgan';
+import routes from './REST/routes';
 
 const app = express();
 app.use(express.static(__dirname + '/public'));
@@ -15,10 +17,32 @@ app.use(express.static('public'));
 
 app.use(cors());
 
+mongoose.connect(config.databaseUrl, {
+ useNewUrlParser: true,
+ useCreateIndex: true,
+ useFindAndModify: false
+}, (error) => {
+ if (error) {
+   console.error(error);
+ }
+ else {
+   console.info('Connect with database established');
+ }
+});
+
+process.on('SIGINT', () => {
+ mongoose.connection.close(function () {
+   console.error('Mongoose default connection disconnected through app termination');
+   process.exit(0);
+ });
+});
+
+routes(app);
+
 app.get('/*', function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
+ res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(config.port, function () {
-  console.info(`Server is running at ${config.port}`)
+ console.info(`Server is running at ${config.port}`)
 });
